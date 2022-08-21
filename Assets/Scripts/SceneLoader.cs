7ea36f83fafffc49c102867aa89894c94c432369
Dominic Griffith <dgriffith6227@sdsu.edu>
2022-08-21 15:26:21 -0700

    Lobby scene and sceneloader prefab

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,8 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public static SceneLoader Instance;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static SceneLoader GetInstance()
+    {
+        return Instance;
+    }
 
     public void LoadSceneByNumber(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void LoadSceneByName(string scene)
     {
         SceneManager.LoadScene(scene);
     }
