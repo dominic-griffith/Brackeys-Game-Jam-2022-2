f8aefb0c691d86620327fa42a5f9251ec91255ed
Dominic Griffith <dgriffith6227@sdsu.edu>
2022-08-27 11:34:48 -0700

    Minor Room update

--- Assets/Scripts/Server/CreateAndJoinRooms.cs
+++ Assets/Scripts/Server/CreateAndJoinRooms.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
