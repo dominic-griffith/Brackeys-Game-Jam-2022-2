be663ea9c6529bd433bc7a6bbc03ee9652a606ef
Dominic Griffith <dgriffith6227@sdsu.edu>
2022-08-27 22:00:45 -0700

    Color change update

--- Assets/PlayerColor.cs
+++ Assets/PlayerColor.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class PlayerColor : MonoBehaviour
 {
-    void Start()
+    private void Update()
     {
-        if (PhotonNetwork.CountOfPlayersInRooms > 1)
-            this.GetComponent<SpriteRenderer>().color = Color.black;
-        else
+        if (PhotonNetwork.IsMasterClient)
             this.GetComponent<SpriteRenderer>().color = Color.white;
+        else
+            this.GetComponent<SpriteRenderer>().color = Color.black;
     }
 
 }
